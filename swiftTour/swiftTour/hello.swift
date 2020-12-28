dc7116ef4dc8758e34e3831bdd07068978233c60
Loyio <i@loyio.me>
2020-12-28 17:07:59 +0800

    first swift project

---
+++ swiftTour/swiftTour/hello.swift
@@ -0,0 +1,17 @@
+//
+//  hello.swift
+//  swiftTour
+//
+//  Created by Loyio on 2020/12/28.
+//
+
+import Foundation
+
+class Dog{
+    func bark() {
+        print("woof")
+        for i in [1,2,3] {
+            print(i)
+        }
+    }
+}
---
+++ swiftTour/swiftTour/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swiftTour
+//
+//  Created by Loyio on 2020/12/28.
+//
+
+import Foundation
+
+let newHello = Dog()
+
+newHello.bark()
+
+print("Hello, World!")
+
