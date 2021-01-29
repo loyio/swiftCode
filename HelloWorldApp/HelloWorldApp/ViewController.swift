9cd5ff2c50e85c4274c3bcd82018725fd14521ca
Loyio <i@loyio.me>
2021-01-29 21:17:44 +0800

    update chapter 3 code

--- HelloWorldApp/HelloWorldApp/ViewController.swift
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let emojiDict: [String: String] = ["👻": "Ghost",
+                                           "🤖️": "Robot",
+                                           "🤪": "Zany face",
+                                           "👑": "Crown"
+                                            ]
+        let selectedButton = sender
+        
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            let meaning = emojiDict[wordToLookup]
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
 }
 
